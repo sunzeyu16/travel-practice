27c705aa54d519a4d052372be67ef2f1f1b41030
sunzeyu16 <sunzeyu16@gmail.com>
2019-03-02 09:46:10 +0800

    add weekend recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -12,13 +12,15 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/HomeWeekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
     HomeIcons: HomeIcons,
-    HomeRecommend: HomeRecommend
+    HomeRecommend: HomeRecommend,
+    HomeWeekend: HomeWeekend
   }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+        title: '玩在宿务玩在宿务玩在宿务玩在宿',
+        desc: '看鲸鲨，亲子游，吃海鲜看鲸鲨，亲子游，吃海鲜'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+        title: '玩在宿务玩在宿务玩在宿务玩在宿',
+        desc: '看鲸鲨，亲子游，吃海鲜看鲸鲨，亲子游，吃海鲜'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+        title: '玩在宿务玩在宿务玩在宿务玩在宿',
+        desc: '看鲸鲨，亲子游，吃海鲜看鲸鲨，亲子游，吃海鲜'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .tiem-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height: .44rem
+      margin-top: .16rem
+      background: #ff9300
+      padding: 0 .2rem
+      border-radius: .06rem
+      color: #fff
+</style>
